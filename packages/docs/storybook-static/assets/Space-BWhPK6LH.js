f47051e811c87bcd7358696fe7bd00f5a0e1c96e
lucas maia <lucasmaia@lucass-MacBook-Air.local>
2025-01-08 14:14:44 -0300

    feat: update storybook-static

---
+++ packages/docs/storybook-static/assets/Space-BWhPK6LH.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{useMDXComponents as n}from"./index-BAvqEBP-.js";import{ae as r}from"./index-CPvTWupa.js";import{T as m}from"./index-CZAfJ17k.js";import{s as i}from"./index-BdGKUmjE.js";import"./index-C9rmetsa.js";import"./index-D7uoVdV3.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-Bi0aG_u3.js";import"../sb-preview/runtime.js";import"./index-D-OUEn-9.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";function s(o){const e={h1:"h1",p:"p",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Tokens/Spaces"}),`
+`,t.jsx(e.h1,{id:"spaces",children:"Spaces"}),`
+`,t.jsx(e.p,{children:"Estas são os espaçamentos do Les UI."}),`
+`,t.jsx(m,{tokens:i,hasRemValue:!0})]})}function D(o={}){const{wrapper:e}={...n(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(s,{...o})}):s(o)}export{D as default};
+//# sourceMappingURL=Space-BWhPK6LH.js.map
