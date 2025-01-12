c16efae49d5c592a159f0826a0a472238c715c8d
lucas maia <lucasmaia@lucass-MacBook-Air.local>
2025-01-11 23:35:39 -0300

    remove storybook-static from repo

--- packages/docs/storybook-static/assets/preview-f31SbyJv.js
+++
@@ -1,7 +0,0 @@
-import{_ as r}from"./iframe-BxXLWn1U.js";import"../sb-preview/runtime.js";var o={docs:{renderer:async()=>{let{DocsRenderer:e}=await r(()=>import("./DocsRenderer-NNNQARDV-DlKxaU5M.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8]));return new e}}};export{o as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-DlKxaU5M.js","assets/iframe-BxXLWn1U.js","assets/index-BEEaWAwe.js","assets/_commonjsHelpers-BosuxZz1.js","assets/client-B8I_qPs-.js","assets/index-D0hyOdTb.js","assets/index-DrPj1_Ln.js","assets/doctrine-CdOZ3mlv.js","assets/index-ogSvIofg.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
