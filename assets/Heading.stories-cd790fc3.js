0a0bf8882eeb6a62283e5d264f248b95c89261c4
programadorcaro <148993279+programadorcaro@users.noreply.github.com>
2025-01-12 21:56:01 +0000

    Deploying to gh-pages from @ programadorcaro/design-system@011067e53a7aa700e0148befcd06ac25b4be7b9b 🚀

--- assets/Heading.stories-cd790fc3.js
+++
@@ -1,6 +0,0 @@
-import{t as i}from"./index-1470439d.js";import"./jsx-runtime-1a9d9a93.js";import"./index-8b3efc3f.js";import"./_commonjsHelpers-de833af9.js";const g={title:"Typography/Heading",component:i,tags:["autodocs"],args:{children:"Heading"},argTypes:{size:{control:"inline-radio",options:["sm","md","lg","xl","2xl","3xl","4xl","5xl"]}}},e={},r={args:{children:"strong text",size:"lg"}};var t,s,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var a,n,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    children: "strong text",
-    size: "lg"
-  }
-}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const u=["Default","CustomText"];export{r as CustomText,e as Default,u as __namedExportsOrder,g as default};
